2784c278759836e93df0fa1d7582ad39c467a2a4
stuartmcr <simcrobbie@gmail.com>
2020-09-07 17:54:15 -0500

    broadcasting.py update

--- code_examples/broadcasting.py
+++ code_examples/broadcasting.py
@@ -6,10 +6,12 @@ A = np.array([[56.0, 0.0, 4.4, 68.0], [1.2, 104.0, 52.0, 8.0], [1.8, 135.0, 99.0
 print(A)
 
 # sum columns
+# axis = 0 means vertical sum, axis = 1 means horizontal sum
 cal = A.sum(axis=0)
 print(cal)
 
 # divide individual values by column sum
+# reshape (1,4) is redundant in this case, but good practice to use it for size verification
 percentage = 100 * A / cal.reshape(1, 4)
 print(percentage)
 
